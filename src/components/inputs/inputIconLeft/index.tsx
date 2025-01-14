65272b28a3d897a055ead81096a6645627d4a506
Gabriel Oliveira <silvatdb8@gmail.com>
2025-01-13 21:37:19 -0300

    feat: criação e estilização do inputIconLeft

---
+++ src/components/inputs/inputIconLeft/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Input, Text, IconElement, Icon } from "@ui-kitten/components"; 
+import { styles } from "./styles";
+
+interface InputGlobalProps {
+  label: string;
+  placeholder: string;
+  secureTextEntry?: boolean;
+  textColor?: string;
+  iconName?: string;
+}
+
+export default function InputIconLeft({
+  label,
+  placeholder,
+  secureTextEntry = false,
+  textColor,
+  iconName,
+}: InputGlobalProps) {
+
+  const icon = (props: any): IconElement => (
+    <Icon
+      {...props}
+      name={iconName || "home"}
+    />
+  );
+
+  return (
+    <>
+      {/* Título (Label) */}
+      <Text>{label}</Text>
+      
+      {/* Input */}
+      <Input
+        style={styles.input}
+        placeholder={placeholder}
+        secureTextEntry={secureTextEntry}
+        placeholderTextColor={textColor}
+        accessoryLeft={icon}
+      />
+    </>
+  );
+}
